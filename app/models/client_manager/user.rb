f36d8f66093962359c2717ab36016d3c3f7cf04b
Timi Ajiboye <timi@helloworld.ng>
2016-09-17 23:03:09 +0100

    cleaned useless functions and callbacks

--- app/models/client_manager/user.rb
+++ app/models/client_manager/user.rb
@@ -7,26 +7,9 @@ module ClientManager
     default_scope { order('created_at DESC') }
     validates :name, :email, presence: true
     validates :email, presence: true, uniqueness: true, email: true
-    # validates_length_of :password, in: 6..20, allow_blank: true
     before_validation :set_temporary_password, unless: :password_exists?
     before_create :send_registration_email
-    # before_save :encrypt_password
-    # after_save :clear_password
-
     has_many :clients
-
-    # def self.authenticate(email="", login_password="")
-    #   user = User.find_by_email(email)
-    #   if user && user.match_password(login_password)
-    #     return user
-    #   else
-    #     return false
-    #   end
-    # end
-
-    # def match_password(login_password="")
-    #   BCrypt::Password.new(self.password) == login_password
-    # end
 
     def client_count
       clients.count
@@ -48,15 +31,6 @@ module ClientManager
       RegistrationMailer.registration_email(self).deliver
     end
 
-    # def encrypt_password
-    #   if password.present?
-    #     self.encrypted_password = BCrypt::Password.create(password)
-    #     puts "encrypt being called"
-    #   end
-    # end
 
-    # def clear_password
-    #   self.password = nil
-    # end
   end
 end
