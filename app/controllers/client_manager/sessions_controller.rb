8909662410acd72dfc0d96e712135c97e4281b20
Timi Ajiboye <timi@helloworld.ng>
2016-10-08 02:45:13 +0100

    Restructure sessions controller to handle all recent changes

--- app/controllers/client_manager/sessions_controller.rb
+++ app/controllers/client_manager/sessions_controller.rb
@@ -4,18 +4,33 @@ module ClientManager
   class SessionsController < ApplicationController
 
     def login
+      redirect_to after_login_path if current_user
+    end
+
+    def logout
+      session.delete(:current_user_id)
+      redirect_to login_path
+    end
+
+    def login_attempt
+      authorized_user = User.find_by(email: params[:email]).try(:authentic ate, params[:password])
+
+      if !authorized_user
+        flash[:error] = "Invalid Username or Password"
+        redirect_to login_path
+      else
+        session[:current_user_id] = authorized_user.id
+        redirect_to after_login_path
+      end
 
     end
 
 
-    def login_attempt
-      authorized_user = User.find_by(email: params[:email]).try(:authenticate, params[:password])
-      if authorized_user
-        flash[:notice] = "Wow Welcome again, you logged in as #{authorized_user.email}"
-        redirect_to users_path
+    def after_login_path
+      if current_user.superadmin
+        return users_path
       else
-        flash[:error] = "Invalid Username or Password"
-        redirect_to login_path
+        return clients_path
       end
     end
 
