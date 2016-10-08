61345bccedd42c570d4ad96ccdc9d99b65c03bf4
Timi Ajiboye <timi@helloworld.ng>
2016-10-08 02:42:59 +0100

    Add routes for password change and logging out

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 ClientManager::Engine.routes.draw do
   root to: 'users#index'
   get 'login', to: 'sessions#login'
+  get 'logout', to: 'sessions#logout'
   post 'login_attempt', to: 'sessions#login_attempt'
+  get 'change_password', to: 'passwords#change'
+  post 'change_password_attempt', to: 'passwords#change_password_attempt'
   get 'clients', to: 'clients#index'
   resources :users, only: [:index, :new, :create]
 end
