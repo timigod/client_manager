31c64a5da65145e5747728f4c5759da6654900f1
Timi Ajiboye <timi@helloworld.ng>
2016-10-09 12:55:12 +0100

    Fix nil id bug in token generation.

--- app/models/client_manager/client.rb
+++ app/models/client_manager/client.rb
@@ -1,8 +1,8 @@
 module ClientManager
   class Client < ApplicationRecord
     belongs_to :user
-    before_validation :generate_token, unless: :token_exists?
-    validates_presence_of :name, :token, :user_id
+    after_create :generate_token, unless: :token_exists?
+    validates_presence_of :name, :user_id
     validate :number_of_users_clients
 
 
@@ -17,7 +17,8 @@ module ClientManager
       if ClientManager.token_secret.blank?
         raise "config.token_secret is missing. Please set it in the client_manager.rb initializer."
       end
-      payload = {client_id: self.id}
+      payload = {client_id: id}
+      puts payload
       token = JWT.encode payload, ClientManager.token_secret, 'HS256'
       self.update(token: token)
     end
